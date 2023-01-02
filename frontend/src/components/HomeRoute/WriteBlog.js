cf6c56dad00d7f3b0cb2e1320acd5781a4618ea8
VaibhMehra <vmp31aug@gmail.com>
2023-01-02 23:47:08 +0530

    log added

--- frontend/src/components/HomeRoute/WriteBlog.js
+++ frontend/src/components/HomeRoute/WriteBlog.js
@@ -6,7 +6,7 @@ export default function TextForm() {
   const navigate = useNavigate();
 
   const handleUpClick = () => {
-    // console.log("UpperCase was clicked " + text);
+    console.log("UpperCase was clicked " + text);
     console.log(text);
     let newtext = text.content.toUpperCase();
     setText(newtext);
