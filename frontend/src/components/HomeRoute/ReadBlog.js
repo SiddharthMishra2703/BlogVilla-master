2f0f79fe2040891ac6c4dc295fa1d0c334bad5fe
Siddharth Mishra <siddharthmishra2703@gmail.com>
2023-04-23 20:06:40 +0530

    conditional rendering for readBlog route

--- frontend/src/components/HomeRoute/ReadBlog.js
+++ frontend/src/components/HomeRoute/ReadBlog.js
@@ -1,7 +1,56 @@
 import React from 'react'
 import Comments from './Comments';
+// import DelButton from "./DelButton";
 // import { Link } from "react-router-dom";
 import { useEffect, useState } from 'react'
+
+function DelButton(props) {
+    console.log(props);
+    if (props.blog == 1) {
+        return <button type="button" onClick={async (e) => {
+
+            e.preventDefault();
+
+            const blogId = props.blogId;
+            try {
+
+                const res = await fetch('/blogDelete', {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        blogId
+                    })
+                });
+
+                const data = await res.json();
+
+                if (res.status === 422 || !data) {
+                    window.alert("Invalid Blog");
+                    console.log("Invalid Blog");
+                } else {
+                    window.alert("Blog saved successfuly");
+                    console.log("Blog saved successfuly");
+
+                    // navigate('/dashboard');
+                }
+
+            } catch (err) {
+                console.log(err);
+                // navigate('/dashboard');
+            }
+            window.location.reload();
+        }} className="btn btn-outline-danger btn-sm mx-2">
+            <i className="zmdi zmdi-delete"></i>
+        </button>
+    }
+}
+
+
+
+
+
 
 
 export default function Blogs() {
@@ -35,6 +84,35 @@ export default function Blogs() {
   }, []);
   const blog = userData;
 
+
+  const [impData, setImpData] = useState({});
+  const getImpData = async () => {
+    try {
+      const res = await fetch('/authenticate/' + blogId, {
+        method: "GET",
+        headers: {
+          Accept: "appllication/json",
+          "Content-Type": "application/json"
+        },
+        credentials: "include"
+      });
+
+      const data = await res.json();
+      setImpData(data);
+
+      if (!res.status === 200) {
+        const error = new Error(res.error);
+        throw error;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  useEffect(() => {
+    getImpData();
+  }, []);
+//   console.log(impData);
+
   return (
     <>
       <div className="card w-50 shadow mx-auto my-5" >
@@ -43,6 +121,8 @@ export default function Blogs() {
           <h6 className="card-subtitle mb-2 text-muted">{blog.topic}</h6>
           <p className="card-text">{blog.content}</p>
           <hr />
+
+          {/* like button */}
           <button type="button" onClick={async (e) => {
 
             e.preventDefault();
@@ -81,43 +161,8 @@ export default function Blogs() {
             <i className="zmdi zmdi-favorite"></i>
             <span>     {blog.likes}</span>
           </button>
-          <button type="button" onClick={async (e) => {
+          <DelButton blog={impData.blog} blogId={blog._id}/>
 
-            e.preventDefault();
-
-            const blogId = blog._id;
-            try {
-
-              const res = await fetch('/blogDelete', {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                  blogId
-                })
-              });
-
-              const data = await res.json();
-
-              if (res.status === 422 || !data) {
-                window.alert("Invalid Blog");
-                console.log("Invalid Blog");
-              } else {
-                window.alert("Blog saved successfuly");
-                console.log("Blog saved successfuly");
-
-                // navigate('/dashboard');
-              }
-
-            } catch (err) {
-              console.log(err);
-              // navigate('/dashboard');
-            }
-            window.location.reload();
-          }} className="btn btn-outline-danger btn-sm mx-2">
-            <i className="zmdi zmdi-delete"></i>
-          </button>
         </div>
       </div>
       <Comments blogId={blog._id} />
