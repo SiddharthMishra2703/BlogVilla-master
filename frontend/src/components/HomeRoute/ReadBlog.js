84343985eae4bed508aad4cb4b184def248fe2f9
VaibhMehra <vmp31aug@gmail.com>
2023-06-02 20:25:55 +0530

    reponsive_readblog

--- frontend/src/components/HomeRoute/ReadBlog.js
+++ frontend/src/components/HomeRoute/ReadBlog.js
@@ -81,6 +81,36 @@ export default function Blogs() {
   const blog = userData;
 
 
+  // For Recommendation Blogs
+  const [userBlogs, setUserBlogs] = useState([]);
+
+  const getBlogs = async () => {
+    try {
+      const res = await fetch('/blog', {
+        method: "GET",
+        headers: {
+          Accept: "appllication/json",
+          "Content-Type": "application/json"
+        },
+        credentials: "include"
+      });
+
+      const data = await res.json();
+      setUserBlogs(data);
+
+      if (!res.status === 200) {
+        const error = new Error(res.error);
+        throw error;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(()=>{
+    getBlogs();
+  }, []);
+
+
   const [impData, setImpData] = useState({});
   const getImpData = async () => {
     try {
@@ -107,15 +137,15 @@ export default function Blogs() {
   useEffect(() => {
     getImpData();
   }, []);
-    // console.log(impData);
+  // console.log(impData);
   const show = () => {
     if (blog.content)
       document.getElementById("content").innerHTML = blog.content;
   }
 
 
-  function CmtDelButton(props){
-    if(props.render){
+  function CmtDelButton(props) {
+    if (props.render) {
       return <button type="button" onClick={async (e) => {
 
         e.preventDefault();
@@ -157,106 +187,110 @@ export default function Blogs() {
 
   return (
     <>
-      <div className='row'>
-        <div className='col-8'>
-          <div className="card shadow mx-3 my-5" >
-            <div className="card-body">
-              <h4 className="card-title">{blog.title}</h4>
-              <h6 className="card-subtitle mb-2 text-muted">{blog.topic}</h6>
-              <p id='content' className="card-text">{show()}</p>
-              <hr />
+      <div className="container-fluid">
+        <div className='row'>
+          <div className='col-md-8'>
+            <div className="card shadow my-5" >
+              <div className="card-body">
+                <h4 className="card-title">{blog.title}</h4>
+                <h6 className="card-subtitle mb-2 text-muted">{blog.topic}</h6>
+                <p id='content' className="card-text">{show()}</p>
+                <hr />
 
-              <div className='d-flex bd-highlight mb-3'>
+                <div className='d-flex bd-highlight mb-3'>
 
-                {/* Blog Like button */}
-                <div className='p-2 bd-highlight'>
-                  <button type="button" onClick={async (e) => {
+                  {/* Blog Like button */}
+                  <div className='p-2 bd-highlight'>
+                    <button type="button" onClick={async (e) => {
 
-                    e.preventDefault();
+                      e.preventDefault();
 
-                    const blogId = blog._id;
-                    try {
+                      const blogId = blog._id;
+                      try {
 
-                      const res = await fetch('/like', {
-                        method: "POST",
-                        headers: {
-                          "Content-Type": "application/json"
-                        },
-                        body: JSON.stringify({
-                          blogId
-                        })
-                      });
+                        const res = await fetch('/like', {
+                          method: "POST",
+                          headers: {
+                            "Content-Type": "application/json"
+                          },
+                          body: JSON.stringify({
+                            blogId
+                          })
+                        });
 
-                      const data = await res.json();
+                        const data = await res.json();
 
-                      if (res.status === 422 || !data) {
-                        window.alert("Invalid Blog");
-                        console.log("Invalid Blog");
-                      } else {
-                        window.alert("Blog saved successfuly");
-                        console.log("Blog saved successfuly");
+                        if (res.status === 422 || !data) {
+                          window.alert("Invalid Blog");
+                          console.log("Invalid Blog");
+                        } else {
+                          window.alert("Blog saved successfuly");
+                          console.log("Blog saved successfuly");
 
-                        // navigate('/dashboard');
+                          // navigate('/dashboard');
+                        }
+
+                      } catch (err) {
+                        console.log(err);
+                        //   navigate('/login');
                       }
+                      window.location.reload();
+                    }} className="btn btn-outline-danger btn-sm">
+                      <i className="zmdi zmdi-favorite"></i>
+                      <span>        {blog.likes}</span>
+                    </button>
+                  </div>
 
-                    } catch (err) {
-                      console.log(err);
-                      //   navigate('/login');
-                    }
-                    window.location.reload();
-                  }} className="btn btn-outline-danger btn-sm">
-                    <i className="zmdi zmdi-favorite"></i>
-                    <span>        {blog.likes}</span>
-                  </button>
+                  {/* Blog delete botton */}
+                  <div className='ms-auto p-2 bd-highlight'>
+                    <DelButton blog={impData.blog} blogId={blog._id} />
+                  </div>
                 </div>
 
-                {/* Blog delete botton */}
-                <div className='ms-auto p-2 bd-highlight'>
-                  <DelButton blog={impData.blog} blogId={blog._id} />
-                </div>
               </div>
+            </div>
 
+            <Comments blogId={blog._id} />
+
+            {/* All Comments section */}
+            <div>
+
+              {blog.comments && blog.comments.map((item) => (
+
+                <div className="card shadow" >
+                  <div className="container-fluid card-body">
+                    <div className='row'>
+                      <div className='col-10 col-md-11'>
+                        <p className="fw-bold card-title">{item.userName}</p>
+                      </div>
+                      <div className='col-2 col-md-1'>
+                        {/* button yaha dalegi */}
+                        <CmtDelButton render={impData.userId == item.userId} commentId={item._id} />
+                        {/* {console.log(impData.userId == item.userId)} */}
+                      </div>
+                    </div>
+                    <p className="card-text">{item.comment}</p>
+                  </div>
+
+                </div>
+              ))}
             </div>
           </div>
+          <div className="col-md-4">
+            <h4 className='text-center my-5'>Recommendation For You</h4>
+            {userBlogs.slice(0,3).map((item)=>(
 
-          <Comments blogId={blog._id} />
-
-          {/* All Comments section */}
-          <div>
-
-            {blog.comments && blog.comments.map((item) => (
-
-              <div className="card shadow mx-3" >
-                <div className="card-body">
-                  <div className='row'>
-                    <div className='col-11'>
-                      <p className="fw-bold card-title">{item.userName}</p>
-                    </div>
-                    <div className='col-1'>
-                      {/* button yaha dalegi */}
-                      <CmtDelButton render={impData.userId == item.userId} commentId={item._id}/>
-                      {/* {console.log(impData.userId == item.userId)} */}
-                    </div>
-                  </div>
-                  <p className="card-text">{item.comment}</p>
-                </div>
-
+            <div className="card shadow mx-auto" >
+              <div className="card-body">
+                <h5 className="card-title">{item.title ? item.title.slice(0, 40) : ""}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">{item.topic}</h6>
+                <p className="card-text" dangerouslySetInnerHTML={{ __html: item.content.slice(0,20) + " ..." }} />
+                {/* <p className="card-text">content...</p> */}
+                <Link to={"/blogs/" + item._id} className="btn btn-sm btn-outline-primary rounded-pill">Read More</Link>
               </div>
+            </div>
             ))}
           </div>
-        </div>
-        <div className="col-4">
-          <h4 className='text-center my-5'>Recommendation For You</h4>
-          {blog.blogs && blog.blogs.map((item)=>(
-          
-          <div className="card shadow my-5" >
-            <div className="card-body">
-              <h5 className="card-title">{item.title ? item.title.slice(0, 40) : ""}</h5>
-              <h6 className="card-subtitle mb-2 text-muted">{item.topic}</h6>
-              {/* <p className="card-text" dangerouslySetInnerHTML={{ __html: item.content.slice(0, 40) + " ..." }} /> */}
-              <Link to={"/blogs/" + item._id} className="btn btn-sm btn-outline-primary rounded-pill">Read More</Link>
-            </div>
-          </div>))}
         </div>
       </div>
     </>
