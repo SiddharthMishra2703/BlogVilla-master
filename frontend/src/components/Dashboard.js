05abbe6516634850de7aba75ff1edf0648467926
Siddharth Mishra <siddharthmishra2703@gmail.com>
2023-06-04 23:31:05 +0530

    responsive dashboard

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import React, {useReducer} from 'react'
 import './css/Dashboard.css'
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
@@ -40,12 +40,9 @@ export default function Dashboard() {
     callDashboardPage();
   }, [change]);
 
-  // state for menu
-  const [open, setOpen] = useState();
-
   return (
 
-    <div className="container-fluid">
+    <div className="container">
       <form method="GET">
         <div className="card shadow text-center" >
           <div className="card-header">
@@ -55,124 +52,123 @@ export default function Dashboard() {
             <h1 className="card-title fw-bolder">Welcome {userData.name} !</h1>
             <p className="card-text fs-5">{userData.work}</p>
             <hr />
-            <div className="linkDisplay row my-4">
-              <div className="col-6 col-md-6 fs-5 fw-bolder">Your ID</div>
-              <div className="col-6 col-md-6 fs-5 text-danger">{userData._id}</div>
+            <div className="row my-4 linkDisplay">
+              <div className="col-6 fs-5 fw-bolder">Your ID</div>
+              <div className="col-6 fs-5 text-danger">{userData._id}</div>
             </div>
             <div className="row my-4">
-              <div className="col-6 col-md-6 fs-5 fw-bolder">Name</div>
-              <div className="col-6 col-md-6 fs-5 text-danger">{userData.name}</div>
+              <div className="col-6 fs-5 fw-bolder">Name</div>
+              <div className="col-6 fs-5 text-danger">{userData.name}</div>
             </div>
             <div className="row my-4">
-              <div className="col-6 col-md-6 fs-5 fw-bolder">Email Address</div>
-              <div className="col-6 col-md-6 fs-5 text-danger">{userData.email}</div>
+              <div className="col-6 fs-5 fw-bolder">Email Address</div>
+              <div className="col-6 fs-5 text-danger">{userData.email}</div>
             </div>
             <div className="row my-4">
-              <div className="col-6 col-md-6 fs-5 fw-bolder">Phone</div>
-              <div className="col-6 col-md-6 fs-5 text-danger">{userData.phone}</div>
+              <div className="col-6 fs-5 fw-bolder">Phone</div>
+              <div className="col-6 fs-5 text-danger">{userData.phone}</div>
             </div>
             <div className="row my-4">
-              <div className="col-6 col-md-6 fs-5 fw-bolder">Work</div>
-              <div className="col-6 col-md-6 fs-5 text-danger">{userData.work}</div>
+              <div className="col-6 fs-5 fw-bolder">Work</div>
+              <div className="col-6 fs-5 text-danger">{userData.work}</div>
             </div>
             <hr />
             <h2 className="card-title">Your Blogs</h2>
 
-            <div className="row my-4">
-              <div className="col-4 col-md-4 fs-5">
+            <div className="row my-4 linkDisplay">
+              <div className="col-md-4 fs-5">
                 <h4>Title</h4>
               </div>
-              <div className="col-4 col-md-4 fs-5">
+              <div className="col-md-4 fs-5">
                 <h4>Topic</h4>
               </div>
-              <div className="col-4 col-md-4 fs-5">
+              <div className="col-md-4 fs-5">
                 <h4>Link</h4>
               </div>
             </div>
 
             {userData.blogs && userData.blogs.map((blog) => (
               <div className="row container-fluid my-3">
-                <div className="col-4 col-md-4 fs-5">
+                <div className="bold-title col-md-4 fs-5">
                   {blog.title}
                 </div>
-                <div className="col-4 col-md-4 fs-5">
+                <div className="col-md-4 fs-5">
                   {blog.topic}
                 </div>
-                {/* menu */}
-                <div className="col-4 col-md-3 fs-5 linkDisplay">
-                  <Link to={"/blogs/" + blog._id} className="btn btn btn-outline-primary">Go To Blog</Link>
+                <div className="col-md-2 fs-5">
+                  <Link to={"/blogs/" + blog._id} className="btn btn-sm btn-outline-primary">Go To Blog</Link>
                 </div>
+                
+                <div className="col-md-2 fs-5">
 
-                <div className="col-4 col-md-1 fs-5">
-                  <div className='d-flex flex-row mb-2'>
-                  <Link to={"/blogs/" + blog._id} className="btn btn-sm btn-outline-primary p-2 me-2 menuDisplay"><i class="zmdi zmdi-open-in-new"></i></Link>
-                    <button type="button" onClick={() => { navigate('/editBlog/' + blog._id); }} className="btn btn-outline-primary btn-sm p-2">
-                      <i className="zmdi zmdi-edit"></i>
-                    </button>
-                    <button type="button" onClick={async (e) => {
-                      const stop = window.confirm("Deleting Your Blog");
-                      if (!stop) {
+                  <button type="button" onClick={ () => {navigate('/editBlog/' + blog._id);}} className="btn btn-outline-primary btn-sm">
+                    <i className="zmdi zmdi-edit"></i>
+                  </button>
+
+                  <button type="button" onClick={async (e) => {
+                    const stop = window.confirm("Deleting Your Blog");
+                    if(! stop){
                         return 0;
+                    }
+
+                    e.preventDefault();
+
+                    const blogId = blog._id;
+                    try {
+
+                      const res = await fetch('/blogDelete', {
+                        method: "POST",
+                        headers: {
+                          "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify({
+                          blogId
+                        })
+                      });
+
+                      const data = await res.json();
+
+                      if (res.status === 422 || !data) {
+                        window.alert("Can not delete blog");
+                        console.log("Can not delete blog");
+                      } else {
+                        forceUpdate();
+                        window.alert("Blog deleted successfuly");
+                        console.log("Blog deleted successfuly");
+                        // navigate('/dashboard');
                       }
 
-                      e.preventDefault();
-
-                      const blogId = blog._id;
-                      try {
-
-                        const res = await fetch('/blogDelete', {
-                          method: "POST",
-                          headers: {
-                            "Content-Type": "application/json"
-                          },
-                          body: JSON.stringify({
-                            blogId
-                          })
-                        });
-
-                        const data = await res.json();
-
-                        if (res.status === 422 || !data) {
-                          window.alert("Can not delete blog");
-                          console.log("Can not delete blog");
-                        } else {
-                          forceUpdate();
-                          window.alert("Blog deleted successfuly");
-                          console.log("Blog saved successfuly");
-                          // navigate('/dashboard');
-                        }
-
-                      } catch (err) {
-                        console.log(err);
-                        navigate('/dashboard');
-                      }
-                      // window.location.reload();
-                    }} className="btn btn-outline-danger btn-sm mx-2 p-2">
-                      <i className="zmdi zmdi-delete"></i>
-                    </button>
-                  </div>
-
+                    } catch (err) {
+                      console.log(err);
+                      navigate('/dashboard');
+                    }
+                    // window.location.reload();
+                  }} className="btn btn-outline-danger btn-sm mx-2">
+                    <i className="zmdi zmdi-delete"></i>
+                  </button>
 
                 </div>
+                <div className='menuDisplay'><hr /></div>
               </div>
+              
             ))}
           </div>
-          <hr />
-          <div className="row container-fluid text-center my-3">
-            <div className="col-3 col-md-3 mx-auto my-4">
+          <hr className='linkDisplay'/>
+          <div className="row text-center my-3">
+            <div className="col-4 col-md-3 mx-auto my-4">
               <Link className="btn btn-outline-primary" to="/writeblog">Write Blog</Link>
             </div>
-            <div className="col-3 col-md-3 mx-auto my-4">
+            <div className="col-4 col-md-3 mx-auto my-4">
               <Link className="btn btn-outline-primary" to="/blogs">Read Now</Link>
             </div>
-            <div className="col-3 col-md-3 mx-auto my-4">
+            <div className="col-4 col-md-3 mx-auto my-4">
               <Link className="btn btn-outline-danger" to="/logout">Log out</Link>
             </div>
-            <div className="col-3 col-md-3 mx-auto my-4">
+            <div className=" col-md-3 mx-auto my-4">
               <button type="button" onClick={async (e) => {
                 const stop = window.confirm("Deleting Your Account");
-                if (!stop) {
-                  return 0;
+                if(! stop){
+                    return 0;
                 }
 
                 e.preventDefault();
